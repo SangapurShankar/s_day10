714a5373e62cc438018b9a6a634b880e3a59a6fd
Good Luck <Roshansangapur@gmail.com>
2022-02-21 21:56:09 +0530

    Day 10 committed

---
+++ s_day10/src/s_day10/Exceptiondemo.java
@@ -0,0 +1,18 @@
+package s_day10;
+
+public class Exceptiondemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			try
+			{
+			int data=50/0;
+			}
+			catch(ArithmeticException e)
+			{
+			System.out.println(e);
+			}
+			System.out.println("rest of code");
+	}
+
+}
